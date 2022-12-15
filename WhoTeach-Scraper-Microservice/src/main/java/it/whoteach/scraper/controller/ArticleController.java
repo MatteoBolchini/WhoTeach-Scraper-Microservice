56228554c8670371d166e6162af6abf07fbd9731
Matteo Bolchini <bolchinimatteo@gmail.com>
2022-12-15 17:13:51 +0100

    15/12 17:12 Aggiunto swagger, messi su cloud tutti i dati sensibili, messo /api nel request dell'article controller

--- WhoTeach-Scraper-Microservice/src/main/java/it/whoteach/scraper/controller/ArticleController.java
+++ WhoTeach-Scraper-Microservice/src/main/java/it/whoteach/scraper/controller/ArticleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.whoteach.scraper.dto.ArticleDto;
@@ -21,6 +22,7 @@ import it.whoteach.scraper.pojo.Article;
 import it.whoteach.scraper.service.ArticleService;
 
 @RestController
+@RequestMapping("/api")
 public class ArticleController {
 
 	@Autowired
