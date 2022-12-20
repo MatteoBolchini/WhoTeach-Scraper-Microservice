8ca70cbc532663571eb665ea1f1e95f4e72ce0f0
Matteo Bolchini <bolchinimatteo@gmail.com>
2022-12-20 17:50:56 +0100

    20/12 17:50 TODO Mockito test

--- WhoTeach-Scraper-Microservice/src/test/java/it/whoteach/scraper/WhoTeachScraperMicroserviceApplicationTests.java
+++ WhoTeach-Scraper-Microservice/src/test/java/it/whoteach/scraper/WhoTeachScraperMicroserviceApplicationTests.java
@@ -1,13 +1,32 @@
 package it.whoteach.scraper;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import it.whoteach.scraper.pojo.Article;
+import it.whoteach.scraper.repository.ArticleRepository;
+import it.whoteach.scraper.service.ArticleService;
 
 @SpringBootTest
 class WhoTeachScraperMicroserviceApplicationTests {
-
+	/*
+	@Autowired
+	ArticleService articleService;
+	
+	@MockBean
+	ArticleRepository articleRepository;
+	
 	@Test
-	void contextLoads() {
-	}
+	public void testPostArticle() {
+		Article article = new Article();
+		article.setUrl("https://www.merlot.org/merlot/viewMaterial.htm?id=123456");
+		article.setSource("Merlot");
+		
+		Mockito.when(articleService.save(article)).thenReturn(article);
+		
+	}*/
 
 }
