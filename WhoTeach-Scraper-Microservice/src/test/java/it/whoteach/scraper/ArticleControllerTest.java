d765941f25c62380dd7ce92dc06a9afac453b6c8
MatteoBolchini <bolchinimatteo@gmail.com>
2022-12-22 14:49:40 +0100

    22/12 14:40 aggiunti altri metodi ad ArticleControllerTest

--- WhoTeach-Scraper-Microservice/src/test/java/it/whoteach/scraper/ArticleControllerTest.java
+++ WhoTeach-Scraper-Microservice/src/test/java/it/whoteach/scraper/ArticleControllerTest.java
@@ -2,13 +2,13 @@ package it.whoteach.scraper;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,22 +20,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.grpc.internal.JsonUtil;
 import it.whoteach.scraper.controller.ArticleController;
+import it.whoteach.scraper.dto.ArticleDto;
 import it.whoteach.scraper.pojo.Article;
 import it.whoteach.scraper.repository.ArticleRepository;
 import it.whoteach.scraper.service.ArticleService;
@@ -60,8 +57,23 @@ public class ArticleControllerTest {
 
 	@Autowired
 	ObjectMapper objectMapper;
-	
+
 	@Test
+	@WithMockUser
+	public void getArticleByIdTest() throws Exception {
+		Article article = new Article();
+		article.setUrl("https://www.javaguides.net/2022/03/spring-boot-unit-testing-crud-rest-api-with-junit-and-mockito.html");
+		article.setSource("Indian Java");
+		article.setId(123456789L);
+
+		when(articleService.getById(any(Long.class))).thenReturn(article);
+		ResultActions response = mockMvc.perform(get("/api/getAll/{ids}", article.getId())
+				.with(csrf()));
+		response.andDo(print()).andExpect(status().isOk());
+
+	}
+
+	//@Test
 	@WithMockUser
 	public void getArticlesByIdTest() throws Exception {
 		List<Article> articles = new ArrayList<>();
@@ -78,14 +90,14 @@ public class ArticleControllerTest {
 		List<Long> ids = new ArrayList<>();
 		ids.add(article1.getId());
 		ids.add(article2.getId());
-		
+
 		when(articleService.findAllById(any(List.class))).thenReturn(articles);
 		ResultActions response = mockMvc.perform(get("/api/getAll/{ids}", article1.getId())
 				.with(csrf()));
 		response.andDo(print()).andExpect(status().isOk());
-		
+
 	}
-	
+
 	@Test
 	@WithMockUser
 	public void getAllArticlesTest() throws Exception {
@@ -122,13 +134,8 @@ public class ArticleControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(article)));
 		response.andDo(print()).andExpect(status().isOk());
-		/*
-		.andExpect(jsonPath("$.url", is(article.getUrl())))
-		.andExpect(jsonPath("$.source", is(article.getSource())))
-		.andExpect(jsonPath("$.id", is(article.getId())));
-		*/
 	}
-	
+
 	@Test
 	@WithMockUser
 	public void postAllArticleTest() throws JsonProcessingException, Exception {
@@ -150,20 +157,40 @@ public class ArticleControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(articles)));
 		response.andDo(print()).andExpect(status().isOk());
+				
 	}
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	@Test
+	@WithMockUser
+	public void updateArticleTest() throws JsonProcessingException, Exception {
+		ArticleDto articleDto = new ArticleDto();
+		articleDto.setUrl("https://www.javaguides.net/2022/03/spring-boot-unit-testing-crud-rest-api-with-junit-and-mockito.html");
+		articleDto.setSource("Indian Java");
+		articleDto.setId(123456789L);
+		ArticleDto articleDto2 = new ArticleDto();
+		articleDto2.setUrl("https://www.facebook.net/2022/03/mockito.html");
+		articleDto2.setSource("Facebook Java");
+		articleDto2.setId(12324245L);
+		
+		when(articleService.update(any(ArticleDto.class))).thenReturn(articleDto.getId());
+		ResultActions response = mockMvc.perform(put("/api/update")
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(articleDto2)));
+		response.andDo(print()).andExpect(status().isOk());
+	}
+	
+	// update all
+	
+	@Test
+	@WithMockUser
+	public void deleteByIdTest() throws Exception {
+		Long articleId = 123456789L;
+		when(articleService.deleteById(any(Long.class))).thenReturn(articleId);
+		ResultActions response = mockMvc.perform(delete("/api/delete/{id}", articleId).with(csrf()));
+		response.andDo(print()).andExpect(status().isOk());
+	}
+	
+	// delete all
+	
 }
